a2e80a63409fd8d3df585a124ca9cf2db78d8a0f
Bekzod <162580380+bekzod221@users.noreply.github.com>
2025-03-08 23:10:10 +0500

    added menu

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AiOutlineThunderbolt } from "react-icons/ai";
 import { IoMdTimer } from "react-icons/io";
 import { IoSettingsSharp } from "react-icons/io5";
+import { IoIosClose } from "react-icons/io";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
+const Burger = ({bool, open}) => {
+  useEffect(() => {
+    AOS.init({
+        duration: 300, 
+        easing: 'ease-in-out-cubic',
+    });
+  }, []);
+  
+  const handleClose = ()=>{
+    bool(false)
+  }
 
-
-
-const Burger = () => {
   return (
-    <div className='fixed inset-0 bg-neutral-900 h-[100vh transform translate-x-0 mb- z-30 px-3 py-6 opacity- duration-300] hidden'>
-      <div className='flex items-center gap-2 border-b-1 border-neutral-600 mb-6'>
-        <img className='h-15 rounded-4xl' src="https://i.pinimg.com/736x/97/28/de/9728defe4bc8dc6a52255a181de7c32e.jpg"/>
-        <div className='flex flex-col'>
-        <h1 className='text-white font-semibold text-[17px] leading-5'>User</h1>
-        <h2 className='font-medium text-neutral-500 text-[15px] cursor-pointer'>View Profile</h2>
+    <div className={`fixed inset-0 bg-neutral-900 h-[100vh] duration-300 mb- z-30 px-3 py-6 transition-all ${open ? 'opacity-100 h-[100vh]' : 'opacity-0 h-0'}`} data-aos="fade-right">
+      <div className='flex justify-between items-center border-b-1 border-neutral-600 mb-3'>
+        <div className='flex items-center gap-2 mb-6'>
+          <img className='h-15 rounded-4xl' src="https://i.pinimg.com/736x/97/28/de/9728defe4bc8dc6a52255a181de7c32e.jpg"/>
+          <div className='flex flex-col'>
+          <h1 className='text-white font-semibold text-[17px] leading-5'>User</h1>
+          <h2 className='font-medium text-neutral-500 text-[15px] cursor-pointer'>View Profile</h2>
+          </div>
+        </div>
+        <div className='text-5xl text-white' onClick={()=> handleClose()}>
+          <IoIosClose/>
         </div>
       </div>
       <div className='flex flex-col gap-2'>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MdMenu } from "react-icons/md";
 import { BsThreeDots } from "react-icons/bs";
 import { GoChevronLeft } from "react-icons/go";
@@ -11,19 +11,26 @@ import Burger from './Burger';
 
 
 
-
-
-
 const Header = () => {
+  const [open, setOpen] = useState(false)
+  
+  const handleMenu = () => {
+    setOpen(true)
+  }
+
   return (
     <>
     <div className='bg-black sticky top-0'>
       <div className='mx-7'>
         <div className='flex justify-between items-center h-14'>
           <div className='flex text-white items-center text-2xl gap-2 cursor-pointer'>
-            <BsThreeDots/>
+          <div className='text-white text-2xl hidden max-[600px]:block' onClick={handleMenu}>
+            <RxHamburgerMenu/>
+          </div>
+          <div className='flex gap-2 items-center max-[600px]:hidden'>
             <GoChevronLeft/>
             <GoChevronRight/>
+          </div>
           </div>
           <div className='flex items-center gap-3'>
             <div className='text-2xl text-white hover:text-gray-400 duration-300 cursor-pointer max-[600px]:hidden'>
@@ -32,21 +39,21 @@ const Header = () => {
             <input className='rounded-3xl bg-white/20 w-[400px] h-11 placeholder-gray-400 font-semibold px-9 text-[14px] text-gray-300 max-[800px]:w-full cursor-pointer max-[600px]:hidden' type="search" name="" id="" placeholder='What do you want to play?' />
           </div>
           <div className='flex gap-4 items-center'>
-          <div className='text-white text-2xl hover:text-gray-400 duration-300 cursor-pointer max-[400px]:hidden'>
+          <div className='text-white text-2xl hover:text-gray-400 duration-300 cursor-pointer'>
             <PiBellRingingFill/>
           </div>
-          <div className='text-white text-2xl hover:text-gray-400 duration-300 cursor-pointer max-[400px]:hidden'>
+          <div className='text-white text-2xl hover:text-gray-400 duration-300 cursor-pointer'>
             <MdOutlineGroups2/>
           </div>
-            <img className='h-9 rounded-[50%] cursor-pointer max-[400px]:hidden' src="https://i.pinimg.com/736x/97/28/de/9728defe4bc8dc6a52255a181de7c32e.jpg"/>
-          <div className='text-white text-2xl hidden max-[400px]:block'>
-            <RxHamburgerMenu/>
-          </div>
+            <img className='h-9 rounded-[50%] cursor-pointer' src="https://i.pinimg.com/736x/97/28/de/9728defe4bc8dc6a52255a181de7c32e.jpg"/>
           </div>
           </div>
         </div>
     </div>
-    <Burger/>
+    {
+      open &&
+      <div><Burger bool={setOpen} open={open}/></div>
+    }
     </>
   )
 }

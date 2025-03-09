698f891477ebd85ba3af9a7b5bba4e227bd9c104
Bekzod <162580380+bekzod221@users.noreply.github.com>
2025-03-10 00:27:46 +0500

    fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import Menu from '../components/Menu'
 
 
 const Home = () => {
+  3
   return (
     <>
       <Sidebar/>
